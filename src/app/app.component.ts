6364e4368680c5c374c4dbf171a9da2cec7dffc5
Selvakumar-18 <selvakumarbalasubramani4@gmail.com>
2024-01-15 12:17:21 +0530

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quizApp';
+  quizData: any;
+  questionS: any;
+  showData:any;
+  colorCode:any;
+  StoreData:any;
+  currentQuestionIndex: number = 0
+  // showNext:Boolean=true;
+
+  constructor(private appService :AppService) {}
+  ngOnInit(){
+    this.getQuestions();
+  }
+
+  getQuestions(){
+    this.appService.getUsers().subscribe((data:any)=>{
+      this.quizData = data;
+    })
+  }
+  sendAns(recivedData:any){
+
+    if(recivedData === true){
+      this.showData = 'Hey Correct Awesome..'
+      this.colorCode = '#2ecc71';
+    }
+    else{
+      this.showData = 'Sorry Try Next time..'
+      this.colorCode = '#e74c3c'
+    }
+  }
+
+  nextQues(){
+    this.currentQuestionIndex++;
+      if (this.currentQuestionIndex <= this.quizData.length) {
+        this.showData='';
+      }
+      else{
+        this.currentQuestionIndex = 0
+      }
+  }
+ 
 }
